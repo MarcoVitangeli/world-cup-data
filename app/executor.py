306fe94f2ebe1e3ba91497b01f191f195898e583
MarcoVitangeli <marco_augusto_vitangeli@outlook.com>
2022-11-30 21:30:32 -0300

    added id caching

--- app/executor.py
+++ app/executor.py
@@ -1,12 +1,11 @@
 from typing import Dict
 import requests
 
-ARGENTINA_ID: int = 9
-
 class RequestExecutor:
     def __init__(self, email: str, password: str, url: str) -> None:
         self.url = url
         self.token = self._get_token(email, password)
+        self.id = None
     
     def _get_teams_url(self) -> str:
         return self.url + 'team'
@@ -35,11 +34,15 @@ class RequestExecutor:
         return self.url + 'standings'
 
     def get_id_by_name(self, name: str) -> int:
+        if self.id != None:
+            return self.id
+
         r = requests.get(self._get_teams_url(), headers=self._get_headers())
         teams = r.json()['data']
 
         for team in teams:
             if team['name_en'].lower() == name.lower():
+                self.id = team['id']
                 return team['id']
 
         raise ValueError('Invalid team entered')
