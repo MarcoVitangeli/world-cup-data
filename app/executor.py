a749301ea69d1ca288981aa5280b15ba7a6d31eb
MarcoVitangeli <marco_augusto_vitangeli@outlook.com>
2022-11-30 21:23:05 -0300

    added all countries to be able to fetch

--- app/executor.py
+++ app/executor.py
@@ -4,9 +4,9 @@ import requests
 ARGENTINA_ID: int = 9
 
 class RequestExecutor:
-    def __init__(self, token: str, url: str) -> None:
-        self.token = token
+    def __init__(self, email: str, password: str, url: str) -> None:
         self.url = url
+        self.token = self._get_token(email, password)
     
     def _get_teams_url(self) -> str:
         return self.url + 'team'
@@ -20,6 +20,41 @@ class RequestExecutor:
                 'Content-Type': 'application/json'
         }
 
-    def get_arg_group(self) -> None:
-        r = requests.get(self._get_team_url(ARGENTINA_ID), headers=self._get_headers())
+    def _get_token(self, email: str, pasw: str) -> str:
+        r = requests.post(self.url + 'user/login', headers={'Content-Type': 'application/json'}, json={
+                'email': email,
+                'password': pasw
+            })
+
+        if r.json()['status'] != 'success':
+            raise RuntimeError('Error login to API')
+
+        return r.json()['data']['token'] 
+
+    def _get_standings_url(self) -> str:
+        return self.url + 'standings'
+
+    def get_id_by_name(self, name: str) -> int:
+        r = requests.get(self._get_teams_url(), headers=self._get_headers())
+        teams = r.json()['data']
+
+        for team in teams:
+            if team['name_en'].lower() == name.lower():
+                return team['id']
+
+        raise ValueError('Invalid team entered')
+
+    def get_group_by_id(self, id: int) -> str:
+        r = requests.get(self._get_team_url(id), headers=self._get_headers())
         return r.json()['data'][0]['groups']
+    
+    def get_pts_by_id(self, teamId: int) -> int:
+        r = requests.get(self._get_standings_url(), headers=self._get_headers())
+        data = r.json()['data']
+        for group in data:
+            for team in group['teams']:
+                if  team['team_id'] == teamId:
+                    return int(team['pts'])
+
+        raise ValueError('Invalid team id')
+
--- app/main.py
+++ app/main.py
@@ -1,18 +1,42 @@
 #!/bin/python3
-import os
 from dotenv import load_dotenv
 from pathlib import Path
 from executor import RequestExecutor
+import argparse, os
+from colorama import Fore, Back, Style
+
+def print_cli(s: str) -> None:
+    print(Fore.WHITE + Back.LIGHTBLUE_EX + Style.DIM + s)
 
 def main() -> None:
-
     load_dotenv(dotenv_path=Path(".env"))
     executor = RequestExecutor(
-                os.environ['API_TOKEN'],
+                os.environ['EMAIL'],
+                os.environ['PASSWORD'],
                 os.environ['API_URL']
             )
+    
+    parser = argparse.ArgumentParser(
+                    prog = 'Informacion del mundial 2022',
+                    description = 'Informacion del mundial 2022 sobre los distintos equipos',
+                    epilog = 'usar -h o --help para ver el uso')
+    
+    parser.add_argument('country')
+    parser.add_argument('-g', '--group', action='store_true')
+    parser.add_argument('-p', '--pts', action='store_true')
 
-    print(executor.get_arg_group())
+    args = parser.parse_args()
+        
+    if args.group:
+        teamId = executor.get_id_by_name(args.country)
+        g: str = executor.get_group_by_id(teamId)
+        print_cli(f'El grupo de {args.country.title()} es el: {g}')
+
+    if args.pts:
+        teamId = executor.get_id_by_name(args.country)
+        pts = executor.get_pts_by_id(teamId)
+        print_cli(f'{args.country.title()} tiene {pts} puntos!')
+
 
 if __name__ == "__main__":
     main()
